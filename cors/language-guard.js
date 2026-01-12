8a6f683269e17fec17903a49ce1ca01f5041a074
Wilson Rodrigues <row654321@gmail.com>
2026-01-12 19:58:24 +0530

    language feature added

---
+++ cors/language-guard.js
@@ -0,0 +1,13 @@
+(function () {
+  // Always show loader immediately
+  if (typeof showLoader === "function") {
+    showLoader();
+  }
+
+  const lang = localStorage.getItem("seva_lang");
+
+  // If language is NOT selected, redirect to language page
+  if (!lang) {
+    window.location.replace("../index.html");
+  }
+})();
